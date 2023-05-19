f0cb49bc4f16f6499d4d2056521b2ee97185c0f8
caeiinos <jeanderoy2001@gmail.com>
2023-05-19 20:21:29 +0200

    domsubtree and form input focus

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -24,7 +24,7 @@ if (QuillBinderIf) {
 
   // store wysiwyg content in input on keyup for binder
   var QuillBinderwys = document.querySelector('#binder-quill');
-  QuillBinderwys.addEventListener('keyup', function() {
+  QuillBinderwys.addEventListener('DOMSubtreeModified', function() {
 
   let QuillBinderValue = document.querySelector('input[name=binderdescription]');
   QuillBinderValue.value = QuilBinder.root.innerHTML.trim();
@@ -55,7 +55,7 @@ if (QuillBinderUIf) {
 
   // store wysiwyg content in input on keyup for binder
   var QuillBinderUwys = document.querySelector('#binder-update');
-  QuillBinderUwys.addEventListener('keyup', function() {
+  QuillBinderUwys.addEventListener('DOMSubtreeModified', function() {
 
   let QuillBinderUValue = document.querySelector('input[name=binderdescriptionupdate]');
   QuillBinderUValue.value = QuilBinderU.root.innerHTML.trim();
@@ -88,7 +88,7 @@ if (QuillNoteIf) {
 
   // store wysiwyg content in input on keyup for note
   var Quillwys = document.querySelector('.oui');
-  Quillwys.addEventListener('keyup', function() {
+  Quillwys.addEventListener('DOMSubtreeModified', function() {
 
     let QuillValueKeeper = document.querySelector('input[name=notedescription]');
     QuillValueKeeper.value = QuillNote.root.innerHTML.trim();
@@ -128,6 +128,7 @@ if (QuillNoteIf) {
 // show add
 const trigger = document.querySelectorAll('.form__trigger');
 const form = document.querySelectorAll('.form');
+const tofocus = document.querySelectorAll('.form__input--tofocus');
 
 for (let i = 0; i < trigger.length; i++) {
   trigger[i].addEventListener("click", show);
@@ -136,6 +137,9 @@ for (let i = 0; i < trigger.length; i++) {
       form[i].classList.remove('form--active');
     } else {
       form[i].classList.add('form--active');
+      if (tofocus[i]) {
+        tofocus[i].focus();
+      }
     }
   }
 }
@@ -166,7 +170,8 @@ for (let i = 0; i < versiontrigger.length; i++) {
 // livesearch
 var headerSearch = document.querySelector(".header__search");
 
-headerSearch.addEventListener('keyup', function() {
+if (headerSearch) {
+  headerSearch.addEventListener('keyup', function() {
     let str = this.value
 
     if (str.length==0) {
@@ -182,7 +187,8 @@ headerSearch.addEventListener('keyup', function() {
       }
       xmlhttp.open("GET","components/livesearch/livesearch.php?q="+str,true);
       xmlhttp.send();
-});
+  });
+}
 
 // livesearch for project explorer
 
